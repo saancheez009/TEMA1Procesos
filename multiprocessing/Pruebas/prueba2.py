3db956aec7d573bfd31a6989dbd8296c53aea089
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2023-11-06 14:41:49 +0100

    si

--- multiprocessing/Pruebas/prueba2.py
+++ multiprocessing/Pruebas/prueba2.py
@@ -10,11 +10,13 @@ def suma(num1,num2):
     print("Suma igual a ",res)
     
 if __name__ == "__main__":
+    inicio=time.time()
     p1 = Process(target = suma, args=(3,5))
     p2 = Process(target=hello, args=("Britany",))
     p1.start()
     p2.start()
     
+
     fin=time.time()
     print("Tiempo:",fin-inicio)
         
