3f784aa8fbe6460bb188daa2aa97cde59105d96a
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2024-03-14 00:00:07 +0100

    .

--- Examen1Rec/app/actores/routes.py
+++ Examen1Rec/app/actores/routes.py
@@ -1,4 +1,46 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
+from utils.functions import escribeFichero, leeFichero
 
 actoresBP = Blueprint('actores',__name__)
+rutaActores="app/ficheros/actores.json"
+rutaPeliculas="app/ficheros/peliculas.json"
+
+@actoresBP.get('/<int:id_actor>')
+def getActor(id_actor):
+    actores = leeFichero(rutaActores)
+    
+    for actor in actores:
+        if actor["id"]== id_actor:
+            return actor,200
+    return{"error" : "Actor no encontrado"},404
+
+@actoresBP.post('/')
+def agregarActor():
+    nuevo_actor = request.json
+    actores = leeFichero(rutaActores)
+    
+    if "PeliculaId" not in nuevo_actor:
+        return {"error": "Falta el campo PeliculaId en el nuevo actor"}, 400
+    
+    # Verifica si el PeliculaId existe en la lista de películas
+    peliculas = leeFichero(rutaPeliculas)
+    if not any(pelicula["id"] == nuevo_actor["PeliculaId"] for pelicula in peliculas):
+        return {"error": "El id de la pelicula no existe en la lista de películas"}, 404
+    
+    actores.append(nuevo_actor)
+    escribeFichero(rutaActores, actores)
+    return nuevo_actor, 201
+
+# Método DELETE para eliminar un actor por su ID
+@actoresBP.delete('/<int:id_actor>')
+def eliminarActor(id_actor):
+    actores = leeFichero(rutaActores)
+    
+    for actor in actores:
+        if actor["id"] == id_actor:
+            actores.remove(actor)
+            escribeFichero(rutaActores, actores)
+            return {"mensaje": "Actor eliminado correctamente"}, 200
+    
+    return {"error": "Actor no encontrado"}, 404
--- Examen1Rec/app/pelicula/routes.py
+++ Examen1Rec/app/pelicula/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 from utils.functions import *
-
+rutaActores="app/ficheros/actores.json"
 rutaPeliculas="app/ficheros/peliculas.json"
 peliculasBP = Blueprint('peliculas', __name__)
 
@@ -13,3 +13,37 @@ def getPelicula(id_pelicula):
         if pelicula["id"]== id_pelicula:
             return pelicula,200
     return{"error" : "Pelicula no encontrada"},404
+
+# Método GET para obtener una película con la lista de actores
+@peliculasBP.get('/<int:id_pelicula>/actores')
+def getPeliculaConActores(id_pelicula):
+    peliculas = leeFichero(rutaPeliculas)
+    actores = leeFichero(rutaActores)
+
+    for pelicula in peliculas:
+        if pelicula["id"] == id_pelicula:
+            actores_pelicula = [actor for actor in actores if actor["idPelicula"] == id_pelicula]
+            if actores_pelicula:
+                pelicula["actores"] = actores_pelicula
+                return pelicula, 200
+            else:
+                return {"error": "No hay actores para esta película"}, 404
+
+        return {"error": "Pelicula no encontrada"}, 404
+    
+@peliculasBP.put('/<int:id_pelicula>')
+def modificarPelicula(id_pelicula):
+    peliculas = leeFichero(rutaPeliculas)
+    nueva_pelicula = request.json
+
+    for pelicula in peliculas:
+        if pelicula["id"] == id_pelicula:
+            pelicula.update(nueva_pelicula)  # Modifica los datos existentes con los datos proporcionados en el JSON
+            escribeFichero(rutaPeliculas, peliculas)
+            return pelicula, 200
+    
+    # Si el ID no existe, añadir un nuevo recurso con los datos pasados en el JSON
+    nueva_pelicula["id"] = id_pelicula
+    peliculas.append(nueva_pelicula)
+    escribeFichero(rutaPeliculas, peliculas)
+    return nueva_pelicula, 201
